38a85b54ad41bd1459b8df7eb7d65ccd29d69b0f
Parzeval <cristian.ramirezp@outlook.com>
2024-02-16 19:54:24 -0500

    reformulando el auth

--- task_manager/urls.py
+++ task_manager/urls.py
@@ -1,12 +1,10 @@
 from django.contrib import admin
 from django.urls import path, include
 from tasks import views
-from django.views.generic.base import TemplateView
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path('accounts/', include('accounts.urls')),
-    path('accounts/', include('django.contrib.auth.urls')),
-    # path('', views.helloworld),
-    path("", TemplateView.as_view(template_name="home.html"), name="home"),
+    path('', views.home, name='home'),
+    path('signup/', views.signup, name='signup'),
+
 ]
--- tasks/views.py
+++ tasks/views.py
@@ -1,5 +1,8 @@
 from django.shortcuts import render
 from django.http import HttpResponse
 
-def helloworld(request):
-    return HttpResponse('Hello World')
+def home(request):
+    return render(request, 'tasks/home.html')
+
+def signup(request):
+    return render(request, 'accounts/signup')
